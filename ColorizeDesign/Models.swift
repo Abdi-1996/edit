d8e2c6b5336afdc0a659c70f884e4ff2d02cdfe6
Abdi <rnz7fnzpj6@privaterelay.appleid.com>
2026-08-27 22:10:20 +0500

    Expand project and production models for Full V3

--- ColorizeDesign/Models.swift
+++ ColorizeDesign/Models.swift
@@ -1,13 +1,43 @@
 import SwiftUI
 
+enum MaterialType: String, Codable, CaseIterable, Identifiable {
+    case acrylic = "Акрил"
+    case pvc = "ПВХ"
+    case acp = "АКП"
+    case aluminum = "Алюминий"
+    case steel = "Сталь"
+    case banner = "Баннер"
+    case vinyl = "Винил"
+    case wood = "Дерево"
+    var id: String { rawValue }
+}
+
+struct ProductionSettings: Codable {
+    var material: MaterialType = .acp
+    var thicknessMM: Double = 3
+    var depthMM: Double = 80
+    var ledEnabled: Bool = false
+    var ledModuleWatt: Double = 0.72
+    var ledSpacingCM: Double = 10
+    var wastePercent: Double = 12
+    var materialPricePerM2: Double = 0
+    var laborPrice: Double = 0
+    var installationPrice: Double = 0
+}
+
 struct SignProject: Identifiable, Codable {
     var id = UUID()
     var name: String
+    var clientName: String = ""
     var widthCM: Double
     var heightCM: Double
     var type: SignType
     var backgroundHex: String = "#FFFFFF"
+    var notes: String = ""
+    var facadeData: Data? = nil
     var elements: [CanvasElement] = []
+    var production = ProductionSettings()
+    var modifiedAt = Date()
 }
 
 enum SignType: String, Codable, CaseIterable, Identifiable {
@@ -17,12 +47,14 @@ enum SignType: String, Codable, CaseIterable, Identifiable {
     case panel = "Композитная панель"
     case window = "Витрина"
     case plate = "Табличка"
+    case pylon = "Пилон"
     var id: String { rawValue }
 }
 
 struct CanvasElement: Identifiable, Codable {
     enum Kind: String, Codable { case text, rectangle, image }
     var id = UUID()
+    var name: String = "Слой"
     var kind: Kind
     var text: String = ""
     var imageData: Data? = nil
@@ -35,6 +67,8 @@ struct CanvasElement: Identifiable, Codable {
     var opacity: Double = 1
     var fillHex: String = "#111111"
     var fontSize: Double = 42
+    var hidden: Bool = false
+    var locked: Bool = false
 }
 
 extension Color {
